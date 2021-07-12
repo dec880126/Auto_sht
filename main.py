0f6d521277dd8f15b459048074e79ce315589f11
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-07-13 06:53:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,49 @@ import requests
 import bs4
 import os
 import time
+
+version = "1.1.5"
+# update note
 '''
-ver 1.0
------update note-----
 ver 1.0 First varsion
+ver 1.1 新增挑出當日更新文章功能    ing...
 '''
-def main(url):    
+
+def today_article(soup):
+    """
+    rtype: list
+    """
+    # today = str(time.strftime("%Y-%m-%d", time.localtime()))
+    today = '2021-07-12'
+    tbody = soup.find_all('tbody')
+    today_list = [] # 存放文章代碼之 list
+
+    # 提取文章代碼
+    for x in tbody:
+        date = x.find('span', attrs={'title':today})
+        if date != None:
+            if date.get('title') != today:
+                continue
+            id = x.get('id')
+            if "normalthread" in str(id):
+                today_list.append(id[-6:])
+            continue          
+    return today_list
+
+def main(url):
+    """
+    type url: str
+    rtype: None
+    """    
     # 發送get 請求 到 sht
     response_of_home = requests.get(url)
 
     # 把HTML 丟入 bs4模組分析
     bs_home = bs4.BeautifulSoup(response_of_home.text,"html.parser")
     
-    # 查找所有html 元素 過濾出 標籤名稱為 'div' 同時class為 title 
+    # 建立今日的新文章清單
+    today_list = today_article(bs_home)
+    # 'a','s xst' 為提取 href 之 html 參數
     titles = bs_home.find_all('a','s xst')
 
     # 萃取文字出來。
@@ -27,17 +57,19 @@ def main(url):
 
     url_of_pages = href_List[0]
     pageNum = 0
-    for url_of_pages in href_List:
-        response_of_pages = requests.get("https://www.sehuatang.org/" + url_of_pages)
+
+    for url_of_pages in today_list:
+        pageNum += 1
+        response_of_pages = requests.get("https://www.sehuatang.org/thread-" + url_of_pages + "-1-1.html")
         bs_pages = bs4.BeautifulSoup(response_of_pages.text,"html.parser")
         magnet = bs_pages.find('div','blockcode').get_text()
         # magnet_List.append(magnet.removesuffix('复制代码'))
-        print(magnet.removesuffix('复制代码'))
-    print("Magnet 已提取完畢")
+        print(str(pageNum) + ". " + magnet.removesuffix('复制代码'))
+    print("Magnet 已提取完畢" + "一共抓取了" + str(pageNum) + "個 magnet")
+    print('[*]===============================================')
     os.system("pause")
 
 if __name__ == '__main__':
-    print(time.strftime("%Y-%m-(%d-1)", time.localtime()))
     while True:        
         URL_List = ["https://www.sehuatang.org/forum-36-1.html", "https://www.sehuatang.org/forum-37-1.html", "https://www.sehuatang.org/forum-2-1.html", "https://www.sehuatang.org/forum-38-1.html", "https://www.sehuatang.org/forum-103-1.html"]
         '''
@@ -49,6 +81,8 @@ if __name__ == '__main__':
         4. 中文: https://www.sehuatang.org/forum-103-1.html
         '''
         print('[*]================== Auto_sht ===================')
+        print("[*]                    v" + version)
+        print('[*]===============================================')
         print("[*]                 1. 無碼")
         print("[*]                 2. 有碼")
         print("[*]                 3. 國產")
@@ -56,6 +90,7 @@ if __name__ == '__main__':
         print("[*]                 5. 中文")
         print("[*]                 6. 結束程式")
         print('[*]===============================================')
+        typeList = ["無碼", "有碼", "國產", "歐美", "中文"]
         typeChoose = int(input("選擇要抓取 Magnet 的版(1~6):"))
         # 處理輸入之 Exception
         if typeChoose < 1 or typeChoose > 6:
@@ -67,6 +102,7 @@ if __name__ == '__main__':
         if typeChoose == 6:
             break
         url_home = URL_List[typeChoose-1]
-        
+        print('[*]===============================================')
+        print("以下為 " + "2021-07-12" + " " + str(typeList[typeChoose-1]) + " 區的 magnet 提取:")
         # 開始抓取
         main(url_home)
