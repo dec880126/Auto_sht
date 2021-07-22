cabfe3f1438837c4ab469cdd693b5d561d05e7f3
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-07-23 03:58:41 +0800

    Update Auto_sht.py

--- Auto_sht.py
+++ Auto_sht.py
@@ -14,7 +14,7 @@ import package.config as config
 
 info = {
     'author': 'CyuanHunag',
-    'version': '5.0.4',
+    'version': '5.0.5',
     'email': 'dec880126@icloud.com',
     'official site': 'https://github.com/dec880126/Auto_sht'
 }
